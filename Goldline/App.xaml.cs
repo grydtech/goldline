e2660b269bccd503c5a3b8a824e5c5b043a8d471
Yasith Jayawardana <yasith1994@live.com>
2017-08-12 18:27:13 +0530

    [Fix] [Todo] Restore user access restrictions

--- Goldline/App.xaml.cs
+++ Goldline/App.xaml.cs
@@ -1,29 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Globalization;
-using System.Windows;
-
-namespace Goldline
+﻿namespace Goldline
 {
     /// <summary>
     ///     Interaction logic for App
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
-        public static IEnumerable<string> GetAllCountries()
-        {
-            var cultureList = new List<string>();
-
-            var cultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
-
-            foreach (var culture in cultures)
-            {
-                var region = new RegionInfo(culture.LCID);
-
-                if (!cultureList.Contains(region.EnglishName))
-                    cultureList.Add(region.EnglishName);
-            }
-            cultureList.Sort();
-            return cultureList;
-        }
     }
 }
